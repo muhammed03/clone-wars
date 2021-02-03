8742fe3a631d402910c0141ecd520fb12f45d191
Muhammedamin <muhammed03amin@gmail.com>
2021-02-03 17:55:09 +0600

    feat: add

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
 
     if (!hashParams.access_token) {
       window.location.href =
-        'https://accounts.spotify.com/authorize?client_id=39974eb923f845789913244fe151d0a5&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=https://spotify-clone-react-rs.herokuapp.com/callback';
+        'https://accounts.spotify.com/authorize?client_id=39974eb923f845789913244fe151d0a5&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=https://muhammed03.github.io/clone-wars/build/index.html/callback';
     } else {
       this.props.setToken(hashParams.access_token);
     }
